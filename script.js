85fc58af94de475dbabf551a9f5559118c1fe4e0
phoenix <iantgaza@gmail.com>
2024-07-18 00:24:27 -0500

    added a func to obtain users input

--- script.js
+++ script.js
@@ -12,16 +12,40 @@
     [ ] create a funtion for 5 rounds
 */
 
-function getComputerChoice() {
-    const weaponArray = ['rock', 'paper', 'scissors'];
+console.log(
+    'Please enter the following into the prompt',
+    "Enter 'rock' or 1 for rock",
+    "Enter 'paper' or 2 for paper",
+    "Enter 'scissors' or 3 for scissors"
+);
 
-    const randomInt = Math.floor(Math.random() * 3);
+
+function getAIChoice() {
+    const weaponArray = ['rock', 'paper', 'scissors'];
+    const randomInt = Math.floor(Math.random() * 3) + 1;
 
     return weaponArray[randomInt];
 }
 
-for (let i = 0; i < 10; i++ ){
-    console.log(getComputerChoice());
+function getUsersChoice() {
+    const getResponse = window.prompt('Please enter either a single number or word');
+
+    const userResponse = parseInt(getResponse) || getResponse.toLowerCase();
+
+    switch (userResponse) {
+        case 'rock':
+        case 1:
+            return 'rock';
+        case 'paper':
+        case 2:
+            return 'paper';        
+        case 'scissors':
+        case 3:
+            return 'scissors';
+        default:
+            return 'losted';
+    }
 }
 
-console.log('hello world')
+
+console.log(getUsersChoice());
