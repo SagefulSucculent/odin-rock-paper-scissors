e4d6e204fd56d1bbcf6fd6dda1fcaed244092ae6
phoenix <iantgaza@gmail.com>
2024-07-17 23:34:03 -0500

    wrote logic for the AIs choice

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+
+/* 
+-=-=-=-=- plan -=-=-=-=-
+    [x] create computer choice
+        
+    [ ] create human imput
+
+    [ ] create scores for each player
+
+    [ ] create a single round
+
+    [ ] create a funtion for 5 rounds
+*/
+
+function getComputerChoice() {
+    const weaponArray = ['rock', 'paper', 'scissors'];
+
+    const randomInt = Math.floor(Math.random() * 3);
+
+    return weaponArray[randomInt];
+}
+
+for (let i = 0; i < 10; i++ ){
+    console.log(getComputerChoice());
+}
+
 console.log('hello world')
