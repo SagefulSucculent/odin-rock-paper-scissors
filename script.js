ece4840585cadabdb689559553020a73ea196157
phoenix <iantgaza@gmail.com>
2024-09-12 21:38:47 -0500

    added a game function that would run the game 5 times, and display the winner

--- script.js
+++ script.js
@@ -7,22 +7,59 @@
 
     [x] create scores for each player
 
-    [ ] create a single round
+    [x] create a single round
 
     [ ] create a funtion for 5 rounds
 */
 
-console.log(
-    'Please enter the following into the prompt',
-    "Enter 'rock' or 1 for rock",
-    "Enter 'paper' or 2 for paper",
-    "Enter 'scissors' or 3 for scissors"
-);
-
 const weaponArray = ['undefined', 'rock', 'paper', 'scissors']
 
-let userScore = 0;
-let aiScore = 0;
+playGame();
+
+function playGame() {
+    let userScore = 0;
+    let aiScore = 0; 
+
+    console.log(
+        'Please enter the following into the prompt',
+        "Enter 'rock' or 1 for rock",
+        "Enter 'paper' or 2 for paper",
+        "Enter 'scissors' or 3 for scissors"
+    );
+
+    for (let game = 0; game < 5; game++ ){
+        console.log('Round ' + (game + 1));
+        playRound();
+    }
+    
+    console.log(
+        'After 5 games',
+        userScore > aiScore ? 
+            `You have won ${userScore}/5 games, Congrats!`:
+            `The AI has won ${aiScore}/5 games, you've doomed all of humanity!!`
+    );
+
+    function playRound() {
+        const aiChoice = getAIChoice();
+        const userChoice = getUsersChoice();
+        console.log('AI choice:', weaponArray[aiChoice] )
+        console.log('User choice:', weaponArray[userChoice] )
+        if(aiChoice === userChoice){
+            console.log('tie');
+        } else if (
+            (aiChoice === 1 && userChoice === 2 ) ||
+            (aiChoice === 2 && userChoice === 3 ) ||
+            (aiChoice === 3 && userChoice === 1 ) 
+        ){
+            console.log('User wins!')
+            userScore++;
+        } else {
+            console.log('AI wins! >:3')
+            aiScore++;
+        }
+    }
+}
+
 function getAIChoice() {
     const randomInt = Math.floor(Math.random() * 3) + 1;
 
@@ -48,25 +85,3 @@ function getUsersChoice() {
             return 0;
     }
 }
-
-function playRound() {
-    const aiChoice = getAIChoice();
-    const userChoice = getUsersChoice();
-    console.log('AI choice:', weaponArray[aiChoice] )
-    console.log('User choice:', weaponArray[userChoice] )
-    if(aiChoice === userChoice){
-        console.log('tie');
-    } else if (
-        (aiChoice === 1 && userChoice === 2 ) ||
-        (aiChoice === 2 && userChoice === 3 ) ||
-        (aiChoice === 3 && userChoice === 1 ) 
-    ){
-        console.log('User wins!')
-        userScore++;
-    } else {
-        console.log('AI wins! >:3')
-        aiScore++;
-    }
-}
-
-playRound();
